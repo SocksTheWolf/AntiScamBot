f1c3227c8658a9cc5dc9584b5ecdf842255f2da0
SocksTheWolf <socks@wolf.stream>
2023-09-28 19:03:34 -0700

    Add metadata versioning for the database

--- BotSetup.py
+++ BotSetup.py
@@ -2,17 +2,24 @@ import os
 import sqlite3
 from Logger import LogLevel, Logger
 
+DATABASE_VERSION=1
+
 def SetupDatabases():
-    if (os.path.exists("bans.db")):
-        Logger.Log(LogLevel.Debug, "Database already exists!")
-        return
-    
+    DatabaseExists:bool = os.path.exists("bans.db")
     Logger.Log(LogLevel.Notice, "Creating database for scam bot setup")
     con = sqlite3.connect("bans.db")
-
     cursor = con.cursor()
-    cursor.execute("CREATE TABLE if not exists banslist(Id, BannerName, BannerId, Date)")
-    cursor.execute("CREATE TABLE if not exists servers(Id, OwnerId, Activated)")
+    CurrentVersion:int = cursor.execute("PRAGMA user_version").fetchone()[0]
+    if (CurrentVersion != 0):
+        # Version updating for the database
+        if (CurrentVersion != DATABASE_VERSION):
+            Logger.Log(LogLevel.Error, f"Please run the database update script from version {CurrentVersion} to {DATABASE_VERSION}")
+        else:
+            Logger.Log(LogLevel.Debug, f"Database version is currently {CurrentVersion}")
+
+    cursor.execute(f"PRAGMA user_version = {DATABASE_VERSION}")
+    cursor.execute("CREATE TABLE if not EXISTS banslist(Id, BannerName, BannerId, Date)")
+    cursor.execute("CREATE TABLE if not EXISTS servers(Id, OwnerId, Activated)")
     con.commit()
     con.close()
     Logger.Log(LogLevel.Notice, "Created the bot databases!")
